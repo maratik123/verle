626164e608f3ac4368e96d11b619a25bbb3dc6b3
Marat Buharov <marat.buharov@gmail.com>
2023-11-21 02:06:09 +0300

    example from softbuffer

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,51 @@
-fn main() {}
+use std::num::NonZeroU32;
+use std::rc::Rc;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let window = Rc::new(WindowBuilder::new().build(&event_loop).unwrap());
+    let context = softbuffer::Context::new(window.clone()).unwrap();
+    let mut surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
+    event_loop
+        .run(move |event, elwt| match event {
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::RedrawRequested,
+            } if window_id == window.id() => {
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width, size.height)
+                };
+                surface
+                    .resize(
+                        NonZeroU32::new(width).unwrap(),
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
+
+                let mut buffer = surface.buffer_mut().unwrap();
+                for y in 0..height {
+                    let green = y % 255;
+                    let offset = y * width;
+                    for x in 0..width {
+                        let red = x % 255;
+                        let blue = (x * y) % 255;
+                        buffer[(offset + x) as usize] = blue | (green << 8) | (red << 16);
+                    }
+                }
+
+                buffer.present().unwrap();
+            }
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::CloseRequested,
+            } if window_id == window.id() => {
+                elwt.exit();
+            }
+            _ => {}
+        })
+        .unwrap();
+}
